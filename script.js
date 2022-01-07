f83cbfac5f1a21f77761c32b1e4121e11cffea88
Kang Jiaming <kangjm2005@hotmail.com>
2022-01-07 19:01:26 +0800

    forbids mobile device

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ document.onkeyup=(e) => {
         document.querySelector(".search-btn").click()
     }
 }
+
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    alert('目前未适配移动设备，请使用电脑端访问')
+    closeCurrentPage()
+  }
+
+
+function closeCurrentPage() {
+	const ua = window.navigator.userAgent;
+	if (ua.indexOf('MSIE') > 0) {
+		if (ua.indexOf('MSIE 6.0') > 0) {
+			window.opener = null;
+			window.close();
+		} else {
+			window.open('', '_top');
+			window.top.close();
+		}
+	} else {
+		window.opener = null;
+		window.open('', '_self', '');
+		window.close();
+	}
+}
